09b5fb6ed2cabbbec4260bc5fd44c8142341d297
Theophilus Adewale <adewaletheophilus@gmail.com>
2023-06-12 17:36:30 +0100

    Added functions for insert, delete and find

--- tree.js
+++ tree.js
@@ -3,7 +3,7 @@ import Node from "./node";
 export default class Tree {
     constructor(arr) {
         this.arr = arr;
-
+        this.root = this.buildTree(arr);
     }
 
     buildTree(arr) {
@@ -29,4 +29,48 @@ export default class Tree {
         return node;
     }
 
+    insert(value, node = this.root){
+        if(node == null) return node;
+
+        if(value < node.value) { node.left = this.insert(value, node.left);
+            } else if (value > node.value) { node.right = this.insert(value, node.right);
+            } else {
+            return node;
+            }
+    } 
+
+    delete(value, node = this.root) {
+        if(node == null) return node;
+
+        if(value < node.value) {
+            node.left = this.delete(node.left);
+        } else if(value > node.value) {
+            node.right = this.delete(value, node.right)
+        } else {
+            //node empty or node with single child
+            if(node.left == null) return node.right;
+            if(node.right == null) return node.left;
+
+            //with 2 children data value
+            node.value = this.minValue(node.right);
+            node.right = this.delete(node.value, node.right);
+        }
+    }
+
+    minValue(node) {
+        let minV = node.value;
+        while(node.left != null) {
+            minV = node.left.value;
+            node = node.left;
+        }
+        return minV;
+    }
+
+    find(value, node = this.root) {
+        if(node.value === value) return node;
+
+        if(value > node.value) return this.find(value, node.right);
+
+        if(value < node.value) return this.find(value, node.left);
+    }
 }
