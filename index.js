773c2974c7909e48ee8ce180f28e9f9a40723582
Theophilus Adewale <adewaletheophilus@gmail.com>
2023-06-13 21:53:11 +0100

    Code Updated and Functionality tested

---
+++ index.js
@@ -0,0 +1,69 @@
+import Tree from "./tree.js";
+
+//Test Code
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if(node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "|   " : "     "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+
+    if(node.left !== null) {
+        prettyPrint(node.left,  `${prefix}${isLeft ? "   " : "|   "}`, true);
+    }
+}
+
+function randomArray(n) {
+    const arr = [];
+    for(let i = 0; i < n; i++) {
+        arr[i] = Math.floor(Math.random() * 101);
+    }
+    return arr;
+}
+
+function addNumbers(n) {
+    const arr = randomArray(n);
+    for(let i = 0; i < n; i++) {
+        tree.insert(arr[i]);
+    }
+}
+
+const tree = new Tree(randomArray(7));//BST from an array of 10 random numbers
+
+prettyPrint(tree.root); //print tree;
+
+// // CHECK IF TREE IS BALANCED
+console.log(`Check If balanced? ${tree.isBalanced()}`); //returns boolean
+
+// //Log elements of Binary Search Tree in Level, pre post and in -- order Traversal
+console.log(`Level order Traversal --> ${tree.levelOrderTraversal()}`); 
+
+console.log(`PreOrder Traversal --> ${tree.preOrderTraversal()}`); 
+
+console.log(`inOrder Traversal--> ${tree.inOrderTraversal()}`); 
+
+console.log(`PostOrder Traversal --> ${tree.postOrderTraversal()}`); 
+
+
+// //ADD A FEW NUMBERS TO BST - to unbalance it
+addNumbers(101); //Adds Random numbers from 1 - 100;
+
+prettyPrint(tree.root); //prints the tree
+
+console.log(`Check Balance After Addition: ${tree.isBalanced()}`); 
+
+//Rebalance BST
+// console.log(`Rebalance Tree`);
+tree.reBalance(); //Re-balance BST;
+
+prettyPrint(tree.root);
+
+console.log(`Confirm if tree is balanced: ${tree.isBalanced()}`); //returns boolean
+
+console.log(`Level Order Traversal: ${tree.levelOrderTraversal()}`);
+
+console.log(`pre-Order Traversal: ${tree.preOrderTraversal()}`);
+
+console.log(`inOrder Traversal: ${tree.inOrderTraversal()}`);
+
+console.log(`postOrder Traversal elements: ${tree.postOrderTraversal()}`);
+
--- tree.js
+++ tree.js
@@ -1,9 +1,13 @@
-import Node from "./node";
+import Node from "./node.js";
 
 export default class Tree {
     constructor(arr) {
         this.arr = arr;
         this.root = this.buildTree(arr);
+        this.levelOrder = [];
+        this.inOrder = [];
+        this.preOrder = [];
+        this.postOrder = [];
     }
 
     buildTree(arr) {
@@ -14,7 +18,7 @@ export default class Tree {
     }
 
     sortDataAndRemoveDuplicate(arr) {
-        const sortedData = [...new Set[arr]].sort((a, b) => a - b);
+        const sortedData = [...new Set(arr)].sort((a, b) => a - b);
         return sortedData;
     }
 
@@ -30,13 +34,15 @@ export default class Tree {
     }
 
     insert(value, node = this.root){
-        if(node == null) return node;
-
-        if(value < node.value) { node.left = this.insert(value, node.left);
-            } else if (value > node.value) { node.right = this.insert(value, node.right);
-            } else {
+        if(node == null) {
+            node = new Node(value); 
             return node;
-            }
+        }
+
+        if(value < node.value) node.left = this.insert(value, node.left);
+         else if (value > node.value) node.right = this.insert(value, node.right);
+        return node;
+            
     } 
 
     delete(value, node = this.root) {
@@ -55,6 +61,7 @@ export default class Tree {
             node.value = this.minValue(node.right);
             node.right = this.delete(node.value, node.right);
         }
+        return node;
     }
 
     minValue(node) {
@@ -72,5 +79,105 @@ export default class Tree {
         if(value > node.value) return this.find(value, node.right);
 
         if(value < node.value) return this.find(value, node.left);
+    }
+
+    levelOrderTraversal(util = this.bstToArray) {
+        this.levelOrder = [];
+        if(this.root === null) return; //do nothing;
+        const queue = [];
+        queue.push(this.root);
+        while(queue.length > 0) {
+            const node = queue[0];
+            util(this.levelOrder, node.value);
+
+            if(node.left != null) queue.push(node.left);
+
+            if(node.right != null) queue.push(node.right);
+            queue.shift()
+        }
+        return this.levelOrder;
+    }
+
+    bstToArray(arr, value) {
+        arr.push(value);
+    }
+
+    //inOrder Travresal
+    inOrderRecursive(util = this.bstToArray, node = this.root) {
+        if(node === null) return; //do nothing
+        //Traverse left subtree
+        this.inOrderRecursive(util, node.left);
+        //root node
+        util(this.inOrder, node.value);
+        //Traverse the right subtree
+        this.inOrderRecursive(util, node.right);
+        return this.inOrder;
+    }
+
+    inOrderTraversal() {
+        this.inOrder = [];
+        return this.inOrderRecursive();
+    }
+
+    //Pre Order Traversal
+    preOrderRecursive(util = this.bstToArray, node = this.root) {
+        if(node === null) return;
+        //visit the root node and log it
+        util(this.preOrder, node.value);
+        //Traverse the left subtree
+        this.preOrderRecursive(util, node.left);
+        //Traverse the right subtree
+        this.preOrderRecursive(util, node.right);
+        return this.preOrder;
+    }
+
+    preOrderTraversal() {
+        this.preOrder = [];
+        return this.preOrderRecursive();
+    }
+
+    //Post Order Traversal
+    postOrderRecursive(util = this.bstToArray, node = this.root) {
+        //base case : tree empty
+        if(node === null) return;
+        this.postOrderRecursive(util, node.left);
+        this.postOrderRecursive(util, node.right);
+        util(this.postOrder, node.value);
+        return this.postOrder;
+    }
+
+    postOrderTraversal() {
+        this.postOrder = [];
+        return this.postOrderRecursive();
+    }
+
+    getHeight(node) {
+        if(node === null) return 0;
+        const heightLeft = this.getHeight(node.left);
+        const heightRight = this.getHeight(node.right);
+        return Math.max(heightLeft, heightRight) + 1;
+    }
+
+    getDepth(value, node = this.root) {
+        if(node.value === value.value) return 0;
+        if(value.value < node.value) return this.getDepth(value, node.left) + 1;
+        if(value.value > node.value) return this.getDepth(value, node.right) + 1;
+    }
+
+    isBalanced() {
+        const totalNodes = this.inOrderTraversal();
+        for(let i = 0; i < totalNodes; i++) {
+            const node = this.find(totalNodes[i]);
+            const leftSubtree = this.getHeight(node.left);
+            const rightSubtree = this.getHeight(node.right);
+            //get the absolute value
+            if (Math.abs(leftSubtree - rightSubtree) < 1) return false;
+        }
+        return true;
+    } 
+
+    reBalance() {
+        const activeTreeArray = this.inOrderTraversal();
+        this.root = this.buildTree(activeTreeArray);
     }
 }
